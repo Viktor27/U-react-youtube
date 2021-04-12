55faca31ea2895d6286b1bdcf2c39b081d8a4b36
_victor_k <viktor.k@360vier.de>
2021-04-12 20:01:10 +0300

    custom hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,21 @@
 import React, {useState, useEffect} from "react";
-import youtube from "../apis/youtube";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
+import useVideos from "../hooks/useVideos";
 
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
-
-  const onTermSubmit = async term => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: term
-      }
-    });
-    console.log(response);
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-  };
+  const [videos, search] = useVideos('hooks');
 
   useEffect(() => {
-    onTermSubmit("WordPress");
-  }, []);
+    setSelectedVideo(videos[0]);
+  }, [videos])
 
   return (
     <div className="ui container">
-      <SearchBar onFormSubmit={onTermSubmit}/>
+      <SearchBar onFormSubmit={search}/>
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
